fdb8358593a8937ae0bbf800a880c81dab95d568
Alexandr Ignatev <ignatyevaleksandr.1992@mail.ru>
2022-12-19 16:19:18 +0000

    Alesia new

--- src/js/main.js
+++ src/js/main.js
@@ -233,9 +233,6 @@ const eventSwiper = new Swiper('.events__slider', {
   a11y: {
     prevSlideMessage: 'предыдущий слайд',
     nextSlideMessage: 'следующий слайд',
-  },
-
-  a11y: {
     paginationBulletMessage: 'Перейти на слайд {{index}}',
   },
 
@@ -267,7 +264,9 @@ function init() {
   var myMap = new ymaps.Map('map', {
     center: [55.75846806898367, 37.60108849999989],
 
-    zoom: 15
+    zoom: 15,
+
+    controls: [],
   });
   var myPlacemark = new ymaps.Placemark([55.75846806898367, 37.60108849999989], {}, {
     iconLayout: 'default#image',
